992317774eac3004c0fe3d4ddb60bcf29bc75ad2
Graziela Diehl <gdiehl@imply.com>
2024-11-27 14:12:13 -0300

    Environment configuration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false,
+};
+
+export const SpotifyConfiguration = {
+  clientId: 'b82d81cab3f34476880a1dbed5ad6799',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    'user-read-currently-playing',
+    'user-read-recently-played',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-top-read',
+    'user-library-read',
+    'playlist-read-private',
+    'playlist-read-collaborative',
+  ],
+};
